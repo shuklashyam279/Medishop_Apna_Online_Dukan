512abdec57549877aaa0dd7117da18aeedcc90c9
Shyam Shukla <shuklashyam279@gmail.com>
2024-09-13 01:14:35 +0530

    Project Initilized

---
+++ src/main/java/com/medishop/MedishopApnaOnlineDukanApplication.java
@@ -0,0 +1,13 @@
+package com.medishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedishopApnaOnlineDukanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedishopApnaOnlineDukanApplication.class, args);
+	}
+
+}
